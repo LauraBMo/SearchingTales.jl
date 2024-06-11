3e663568ed5c1edf0517d5ac4bd5146e7469a799
Documenter.jl <documenter@juliadocs.github.io>
2024-06-11 14:33:05 +0000

    build based on b10c149

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SearchingTales","category":"page"},{"location":"#SearchingTales","page":"Home","title":"SearchingTales","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SearchingTales.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SearchingTales]","category":"page"},{"location":"#SearchingTales.FubiniStudy","page":"Home","title":"SearchingTales.FubiniStudy","text":"FubiniStudy <: Metric\nrmse(x, y)\n\nFubini Study distance in complex projective plane PC^2\n\n\n\n\n\n","category":"type"}]
+}
